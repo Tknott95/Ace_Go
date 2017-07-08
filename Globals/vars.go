8066cfc69303bc6ec34ecf2bd6856b0ac1a4f229
tknott95 <tknot95@hotmail.com>
2017-07-07 18:24:53 -0600

    Adding with cur date - TK

--- Globals/vars.go
+++ Globals/vars.go
@@ -2,11 +2,10 @@ package globals
 
 import (
 	"html/template"
-	"time"
 )
 
 var PortNumber string = ":" + "8080"
 
 var Tmpl = template.Must(template.ParseGlob("./Views/*"))
 
-var CurTime = time.Now()
+// var CurTime = time.Now()
