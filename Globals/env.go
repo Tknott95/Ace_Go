92d6d4ac6654ab4b0a3b81570ccb63d5b3576604
tknott95 <tknot95@hotmail.com>
2017-07-10 02:47:16 -0600

    Git - TK

--- Globals/env.go
+++ Globals/env.go
@@ -14,3 +14,4 @@ var TwilioAuthToken = "3f96cfd74a3fd9fe91e670aca9635183"
 var SGridApi = "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"
 
 // var CurTime = time.Now()
+jjhcjhvj,hv
