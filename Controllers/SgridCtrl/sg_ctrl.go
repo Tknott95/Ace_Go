29e4e56b67b2789e3e46434928bf8dc222002230
tknott95 <tknot95@hotmail.com>
2017-07-10 02:23:31 -0600

    Email Link made foevaa - TK

--- Controllers/SgridCtrl/sg_ctrl.go
+++ Controllers/SgridCtrl/sg_ctrl.go
@@ -13,12 +13,24 @@ import (
 )
 
 func SendEmail(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	var fromWho string
+	var subjOfMail string
 	var mailToSend string
 
+	fromWho = req.FormValue("mail-from")
+	subjOfMail = req.FormValue("mail-subj")
 	mailToSend = req.FormValue("mail-to-trev")
 
-	from := mail.NewEmail("TK - From AceAdmin", "tknott95@hotmail.com")
-	subject := "Sent From AceAdmin via. Sendgrid"
+	// if subjOfMail == "" {
+	// 	subjOfMail = "Sent From AceAdmin via. Sendgrid"
+	// }
+
+	// if fromWho == "" {
+	// 	fromWho = "Anonymous@trevorknott.io"
+	// }
+
+	from := mail.NewEmail("TK - From AceAdmin", fromWho)
+	subject := subjOfMail
 	to := mail.NewEmail("TK - From AceAdmin", "tk@trevorknott.io")
 	plainTextContent := mailToSend
 	htmlContent := "<strong>..." + mailToSend + "</strong>"
@@ -28,8 +40,13 @@ func SendEmail(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	if err != nil {
 		log.Println(err)
 	} else {
+		println("Response StatusCode")
 		fmt.Println(response.StatusCode)
-		fmt.Println(response.Body)
+		println("\n" + "Response Body")
+		fmt.Println(response.Body + "\n")
+		println("Response Headers")
 		fmt.Println(response.Headers)
 	}
+
+	http.Redirect(w, req, "/", 301)
 }
