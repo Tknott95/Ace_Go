34118d820ff28ce85ded5f8942ab6637a823092e
tknott95 <tknot95@hotmail.com>
2017-07-10 02:43:33 -0600

    SGrid way more complex now -__- - TK

--- Controllers/SgridCtrl/sg_ctrl.go
+++ Controllers/SgridCtrl/sg_ctrl.go
@@ -3,13 +3,13 @@ package sGrid_Ctrl
 import (
 	"fmt"
 	"log"
-	"os"
 
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 	sendgrid "github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
+	globals "github.com/tknott95/MasterGo/Globals"
 )
 
 func SendEmail(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
@@ -21,13 +21,13 @@ func SendEmail(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	subjOfMail = req.FormValue("mail-subj")
 	mailToSend = req.FormValue("mail-to-trev")
 
-	// if subjOfMail == "" {
-	// 	subjOfMail = "Sent From AceAdmin via. Sendgrid"
-	// }
+	if subjOfMail == "" {
+		subjOfMail = "Sent From AceAdmin via. Sendgrid"
+	}
 
-	// if fromWho == "" {
-	// 	fromWho = "Anonymous@trevorknott.io"
-	// }
+	if fromWho == "" {
+		fromWho = "Anonymous@trevorknott.io"
+	}
 
 	from := mail.NewEmail("TK - From AceAdmin", fromWho)
 	subject := subjOfMail
@@ -35,15 +35,15 @@ func SendEmail(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	plainTextContent := mailToSend
 	htmlContent := "<strong>..." + mailToSend + "</strong>"
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
-	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	client := sendgrid.NewSendClient(globals.SGridApi)
 	response, err := client.Send(message)
 	if err != nil {
 		log.Println(err)
 	} else {
 		println("Response StatusCode")
 		fmt.Println(response.StatusCode)
-		println("\n" + "Response Body")
-		fmt.Println(response.Body + "\n")
+		println("Response Body")
+		fmt.Println(response.Body)
 		println("Response Headers")
 		fmt.Println(response.Headers)
 	}
