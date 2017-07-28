57fad2d041dd4c9b7903208521ccbee2eca49f52
Tknott95 <tknott95@hotmail.com>
2017-07-27 23:18:06 -0600

    Vuews Refactored - TK

--- Globals/env.go
+++ Globals/env.go
@@ -6,7 +6,7 @@ import (
 
 var PortNumber string = ":" + "8080"
 
-var Tmpl = template.Must(template.ParseGlob("./Views/*"))
+var Tmpl = template.Must(template.ParseGlob("./Views/**/*"))
 
 var TwilioSID = "ACc2be7e8f4aac8fccf91ee7ae1e51c779"
 var TwilioAuthToken = "3f96cfd74a3fd9fe91e670aca9635183"
